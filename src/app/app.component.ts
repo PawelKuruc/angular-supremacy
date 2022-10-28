e64978ce1b7a24813dc5ef97059324e26a5fbee2
PawelKuruc <p.kuruc.of@gmail.com>
2022-10-29 01:16:54 +0200

    some css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,6 @@ import { User } from './data-model/User';
 export class AppComponent {
   title = 'angular-supremacy';
 
-  constructor(){
-    var user1: User = new User('Paweł Kuruc', 'orange');
-    var user2: User = new User('Eryk Pałasz', 'blue');
-
-    var task1: Task = new Task('Doing things', '');
-    var task2: Task = new Task('Something important', '', user1);
-
-    task1.assign(user2);
-    task2.setName('Collecting data');
-    task2.setDescription('This is a task about collecting some important data');
-    task2.assign(user2);
-    task2.removeAssignment();
-    task1.showTaskHistory();
-    task2.showTaskHistory();
-  }
+  constructor(){}
 
 }
